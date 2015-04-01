6fe2c6fccffba9b7c354039343657165daac768e
ephys <ephyspotato@gmail.com>
2015-04-01 13:45:33 +0200

    Placeholder page

---
+++ assets/javascript/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function() {
+	var introSection = document.getElementById('intro');
+
+	var h1 = document.createElement('h1');
+	introSection.appendChild(h1);
+
+	slowPrint(h1, location.hostname + '_');
+})();
+
+function slowPrint(element, text, delay) {
+	delay = delay || 50;
+
+	var indice = 0;
+
+	var timeout = function() {
+		element.innerText += text.charAt(indice++);
+
+		if (indice !== text.length) {
+			setTimeout(timeout, delay);
+		}
+	};
+
+	setTimeout(timeout, delay);
+}
