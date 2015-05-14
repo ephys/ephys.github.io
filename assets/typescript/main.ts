51ac3549c83f50bed6f5a69023c6f17ac0703cfd
ephys <ephyspotato@gmail.com>
2015-05-14 16:44:33 +0200

    merged iconbase & iconhover to avoid having the icon disappear while it's loading on hover

--- assets/typescript/main.ts
+++ assets/typescript/main.ts
@@ -1,20 +1,20 @@
 'use strict';
 
 // replace SVG images by inlines (http://stackoverflow.com/questions/11978995/how-to-change-color-of-svg-image-using-css-jquery-svg-image-replacement)
-(function() {
-	$('img[src$=".svg"]').each(function() {
-		var img = this;
-		var imgURL = this.src;
-
-		$.get(imgURL, function(data) {
-			var $svg = $(data).find('svg');
-			var svg = $svg[0];
-
-			svg.id = img.id;
-			svg.setAttribute('class', img.getAttribute('class'));
-
-			$(svg).removeAttr('xmlns:a');
-			$(img).replaceWith($svg);
-		}, 'xml');
-	});
-})();
+//(function() {
+//	$('img[src$=".svg"]').each(function() {
+//		var img = this;
+//		var imgURL = this.src;
+//
+//		$.get(imgURL, function(data) {
+//			var $svg = $(data).find('svg');
+//			var svg = $svg[0];
+//
+//			svg.id = img.id;
+//			svg.setAttribute('class', img.getAttribute('class'));
+//
+//			$(svg).removeAttr('xmlns:a');
+//			$(img).replaceWith($svg);
+//		}, 'xml');
+//	});
+//})();
