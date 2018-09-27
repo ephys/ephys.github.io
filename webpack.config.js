624c719b37d8208888d7292d164f6e66c32798b7
Guylian Cox <gc@madkings.com>
2018-09-27 21:56:42 +0200

    build: transpile javascript using babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, 'assets/js'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
